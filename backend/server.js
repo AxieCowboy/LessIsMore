f1a81bd0197d585205fe84e4cb4877f648e70b17
AxieCowboy <hmhroko@gmail.com>
2025-03-18 10:06:48 -0600

    removing node modules

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ require('dotenv').config()
 const app = express()
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 
 // MongoDB Connection
